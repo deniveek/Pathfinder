4edd4d46f715c1fe20e77f0e9dce848741e60c75
deniveek <71970812+deniveek@users.noreply.github.com>
2022-10-21 09:46:08 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,163 +1,257 @@
 import sys
 import numpy as np
-import json
-import RDP
-import pathfinder
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+from operator import xor
+import RDP
 
 
-class Map():
-	def __init__(self):
-		self.walls = []
+class Boundary:
+    def __init__(self, boundary):
+        self.boundary = boundary
 
-	def add_wall(self, wall: 'ndarray'):
-		print(len(wall))
-		wall_ = RDP.DouglasPeucker(wall)
-		print(len(wall_))
-		self.walls.append(wall_)
+    @property
+    def points(self):
+        return self.boundary[0], self.boundary[1]
 
 
-class Test(QWidget):
-	def __init__(self):
-		super(Test, self).__init__()
-		self.setGeometry(100, 200, 600, 400)
-		self.setWindowTitle("test")
-		self.drawing = 0
-		self.line = []
-		self.path = []
-		self.queue = False
-		self.obstacles = Map()
-		#self.loadMap()
-		self.start_point = np.array([100, 100])
-		self.fin_point = np.array([500, 300])
-		self.show()
+class Map:
+    def __init__(self):
+        self.robot_pos = None
+        self.destination = None
+        self.boundaries = []
+        self.path = None
 
-	def loadMap(self):
-		try:
-			with open("map.json", "r") as map:
-				json_object = json.load(map)
-				for wall in json_object:
-					self.obstacles.add_wall(np.array(wall))
-		except:
-			print("no map file")
-		# Update bbox
-		#self.bbox = pathfinder.AugmentBBox(pathfinder.BBox(self.obstacles.walls), 1.1)
-		return 0
+    def add_wall(self, wall):
+        wall = RDP.DouglasPeucker(wall)
+        res = np.stack([wall[:-1], wall[1:]], axis=1)
+        for wall in res:
+            self.boundaries.append(wall)
+        return 0
 
-	def findPath(self):
-		#self.bbox = pathfinder.AugmentBBox(pathfinder.BBox(self.obstacles.walls), 1.1)
-		self.path = pathfinder.findPath(self.obstacles.walls, self.start_point, self.fin_point)
-		self.update()
+    def add_robot(self, pos):
+        self.robot_pos = pos
+        return 0
 
-	def qtpoint2ndarray(self, point):
-		return np.array([point.x(), point.y()])
+    def add_destination(self, pos):
+        self.destination = pos
+        return 0
 
-	def paintEvent(self, event):
-		qp = QPainter()
-		qp.begin(self)
+    @classmethod
+    def intersects(cls, s0p0, s0p1, s1p0, s1p1):
+        return xor(cls.is_right(s0p0, s0p1, s1p0), cls.is_right(s0p0, s0p1, s1p1)) and xor(cls.is_right(s1p0, s1p1, s0p0), cls.is_right(s1p0, s1p1, s0p1))
 
-		# Move coords to LeftTop of BBox
-		#vp = pathfinder.AugmentBBox(self.bbox, 1.1)
-		#qp.setWindow(vp[0], vp[1], vp[2] - vp[0], vp[3] - vp[1])
+    @staticmethod
+    def is_right(p0, p1, p):
+        ab = p1 - p0
+        ac = p - p0
+        return True if np.cross(ac, ab) > 0 else False
 
-		# Draw obstacles
-		qp.setPen(QColor(Qt.red))
-		for wall in self.obstacles.walls:
-			qp.drawPolyline(QPolygon([QPoint(p[0], p[1]) for p in wall]))
-		qp.drawPolyline(QPolygon(self.line))
+    @classmethod
+    def is_visible(cls, edges, p0, p1):
+        #print(f"checking if visible for {p0}, {p1}")
+        if len(edges) > 0:
+            closing = next((s for s in edges if cls.intersects(p0, p1, s[0], s[1])), None)
+            #print('closing')
+            return closing is None
+        return True
 
-		# Draw start and stop points
-		qp.setPen(QColor(Qt.blue))
-		qp.drawEllipse(QPoint(self.start_point[0], self.start_point[1]), 5, 5)
-		qp.drawEllipse(QPoint(self.fin_point[0], self.fin_point[1]), 5, 5)
 
-		if len(self.path):
-			qp.setPen(QColor(Qt.green))
-			qp.drawPolyline(QPolygon([QPoint(p[0], p[1]) for p in self.path]))
+class Vertex:
+    def __init__(self, pos, parent=None):
+        self.pos = pos
+        self.parent = parent
 
-		# Draw BBOX
-		#qp.setPen(QColor(Qt.magenta))
-		#qp.drawRect(self.bbox[0], self.bbox[1], self.bbox[2]-self.bbox[0], self.bbox[3]-self.bbox[1])
+    def __eq__(self, other):
+        return np.array_equal(self.pos, other)
 
-		qp.end()
 
-	def mousePressEvent(self, event):
-		if event.button() == Qt.RightButton:
-			self.queue = event.pos()
-		elif event.button() == Qt.LeftButton:
-			if self.drawing == 1:
-				self.line = []
-				self.line.append(event.pos())
-				self.drawing = 2
-			elif np.linalg.norm(self.qtpoint2ndarray(event.pos()) - self.start_point) <= 5:
-				self.drawing = 3
-			elif np.linalg.norm(self.qtpoint2ndarray(event.pos()) - self.fin_point) <= 5:
-				self.drawing = 4
+class Edge:
+    def __init__(self, p1, p2, l):
+        self.p1 = p1
+        self.p2 = p2
+        self.length = l
 
-	def mouseReleaseEvent(self, event):
-		if event.button() == Qt.LeftButton:
-			if self.drawing == 2:
-				self.obstacles.add_wall(np.array([[p.x(), p.y()] for p in self.line]))
-				self.line = []
-				self.drawing = 1
-			elif self.drawing == 3 or self.drawing == 4:
-				self.drawing = 0
 
-	def mouseMoveEvent(self, event):
-		if self.drawing == 2:
-			self.line.append(event.pos())
-			self.update()
-		elif self.drawing == 3:
-			self.start_point = self.qtpoint2ndarray(event.pos())
-		elif self.drawing == 4:
-			self.fin_point = self.qtpoint2ndarray(event.pos())
+class Tree:
+    def __init__(self, startpoint):
+        self.vertices = [startpoint]
+        self.edges = []
 
-	def keyPressEvent(self, event):
-		if event.key() == Qt.Key_Alt and self.drawing == 0:
-			self.drawing = 1
-			#print('alt')
-		elif event.key() == Qt.Key_S:
-			to_file = [wall.tolist() for wall in self.obstacles.walls]
-			file = json.dumps(to_file)
-			with open("map.json", "w") as output:
-				output.write(file)
-			print('map saved')
-		elif event.key() == Qt.Key_C:
-			self.obstacles = Map()
-			print('c event')
-		elif event.key() == Qt.Key_L:
-			self.loadMap()
-		elif event.key() == Qt.Key_F:
-			self.findPath()
+    def add_vertex(self, pos, parent=None):
+        self.vertices.append(Vertex(pos, parent))
 
-	def keyReleaseEvent(self, event):
-		if event.key() == Qt.Key_Alt:
-			self.line = []
-			self.drawing = 0
-			#print('not alt')
+    def add_edge(self, p1, p2, l):
+        self.edges.append(Edge(p1, p2, l))
 
-	def closest_wall(self, point):
-		dist_to_wall = 9999
-		for wall in self.obstacles.walls:
-			col = [Test.dist(point, p[0], p[1]) for p in list(zip(wall, wall[1:])) if np.cross(p[0], point) * np.cross(p[1], point) < 0]
-			if len(col) and min(col) < dist_to_wall:
-				dist_to_wall = min(col)
-		return dist_to_wall
 
-	@classmethod
-	def dist(cls, point, start, end):
-		if np.all(np.equal(start, end)):
-			return np.linalg.norm(point - start)
-		return np.divide(np.abs(np.linalg.norm(np.cross(start - end, start - point))), np.linalg.norm(end - start))
+class RRT:
+    def __init__(self, start, goal, obstacles, bounds=(800, 640), u=30, k=3000):
+        self.u = u
+        self.k = k
+        self.bounds = bounds
+        self.obstacles = obstacles
+        self.rng = np.random.default_rng(12345)
+        self.trees = {}
+        self.x_start = start
+        self.x_goal = goal
+        self.x_new = None
+        #Tree(self.x_start)
+        pass
+
+    def build_RRT(self):
+        print("started building rrt")
+        self.trees['start'] = Tree(self.x_start)
+        for i in range(self.k):
+            x_rand = self.rng.integers([0, 0], [self.bounds], 2)
+            print(f"iteration number {i}, sampling: {x_rand}")
+            self.extend(self.trees['start'], x_rand)
+        print('rrt built')
+        return self.trees['start']
+
+    def bidirRRT(self):
+        print("started bidirectional RRT")
+        self.trees.update({'T_a': Tree(Vertex(self.x_start)), 'T_b': Tree(Vertex(self.x_goal))})
+        active_tree = 'T_a'
+        second_tree = 'T_b'
+        for i in range(self.k):
+            x_rand = self.rng.integers([0, 0], [self.bounds], 2)
+            print(f"iteration number {i}, sampling: {x_rand}")
+            if not self.extend(self.trees[active_tree], x_rand) == "Trapped":
+                if self.extend(self.trees[second_tree], self.x_new) == "Reached":
+                    #print(f"{self.trees[active_tree].vertices[-1].pos}, {self.trees[second_tree].vertices[-1].pos}")
+                    #print('reached')
+                    return self.full_path(self.trees[active_tree], self.trees[second_tree])
+            second_tree = active_tree
+            active_tree = 'T_a' if active_tree == 'T_b' else 'T_b'
+        print("finished bidirectional RRT, couldn't find a route tho")
+
+    def extend(self, tree, x):
+        x_near = self.nearest_neighbor(tree, x)
+        #print(f"found nearest neighbor: {x_near.pos}")
+        try:
+            self.x_new, u_new = self.new_state(x, x_near.pos)
+            #print(f"found new state: {self.x_new}")
+            tree.add_vertex(self.x_new, x_near)
+            #tree.add_edge(x_near, self.x_new, u_new)
+            #print("new state added to the tree")
+            return "Reached" if np.array_equal(self.x_new, x) else "Advanced"
+        except:
+            return "Trapped"
+
+    def new_state(self, x, x_near):
+        x_new = np.int_(x_near + (x - x_near)/np.linalg.norm(x - x_near) * self.u) if self.u < np.linalg.norm(x - x_near) else x
+        if Map.is_visible(self.obstacles, x_new, x_near):
+            #print('visible')
+            return x_new, self.u
+        else:
+            #print('obscured')
+            return None
+
+    @staticmethod
+    def nearest_neighbor(tree, x):
+        #print(f"here {tree.vertices}, x: {x}")
+        return tree.vertices[np.argmin(np.array([np.linalg.norm(x - v.pos) for v in tree.vertices]))]
+
+    @staticmethod
+    def path(T):
+        path_a = [np.array([T.vertices[-1].pos, T.vertices[-1].parent.pos])]
+        next_v = T.vertices[-1].parent
+        while next_v.parent is not None:
+            path_a.append(np.array([next_v.pos, next_v.parent.pos]))
+            next_v = next_v.parent
+        #print(path_a)
+        return path_a
+
+    def full_path(self, T_a, T_b):
+        path1 = self.path(T_a)
+        path2 = self.path(T_b)
+        #print("path ready")
+        return path1 + path2
+
+
+class Window(QWidget):
+    def __init__(self):
+        super(Window, self).__init__()
+        self.setWindowTitle("pathfinder")
+        self.setFixedSize(QSize(800, 640))
+        self.world = Map()
+        self.state = 0
+        self.line = []
+        self.path = []
+        #self.loadMap()
+        self.show()
+
+    @staticmethod
+    def qtpoint2ndarray(point):
+        return np.array([point.x(), point.y()])
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            if self.state == 0:
+                self.line = []
+                self.line.append(event.pos())
+                self.state = 1
+        elif event.button() == Qt.RightButton:
+            if self.world.robot_pos is None:
+                pos = self.qtpoint2ndarray(event.pos())
+                self.world.add_robot(pos)
+            else:
+                pos = self.qtpoint2ndarray(event.pos())
+                self.world.add_destination(pos)
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            if self.state == 1:
+                self.world.add_wall([self.qtpoint2ndarray(p) for p in self.line])
+                self.line = []
+                self.state = 0
+        self.update()
+
+    def mouseMoveEvent(self, event):
+        if self.state == 1:
+            self.line.append(event.pos())
+        self.update()
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_F:
+            #print(self.world.is_visible(self.world.boundaries, self.world.robot_pos, self.world.destination))
+            pathfinder = RRT(self.world.robot_pos, self.world.destination, self.world.boundaries)
+            self.world.path = pathfinder.bidirRRT()
+            #print(self.world.path)
+        self.update()
+
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+
+        qp.setPen(QColor(Qt.red))
+        qp.drawPolyline(QPolygon(self.line))
+        for wall in self.world.boundaries:
+            p1, p2 = wall[0], wall[1]
+            qp.drawPolyline(QPoint(p1[0], p1[1]), QPoint(p2[0], p2[1]))
+        qp.drawPolyline(QPolygon(self.line))
+
+        if self.world.robot_pos is not None:
+            qp.setPen(QColor(Qt.blue))
+            qp.drawEllipse(QPoint(self.world.robot_pos[0], self.world.robot_pos[1]), 5, 5)
+        if self.world.destination is not None:
+            qp.setPen(QColor(Qt.magenta))
+            qp.drawEllipse(QPoint(self.world.destination[0], self.world.destination[1]), 5, 5)
+        if self.world.path is not None:
+            qp.setPen(Qt.darkGreen)
+            for edge in self.world.path:
+                qp.drawPolyline(QPoint(edge[0][0], edge[0][1]), QPoint(edge[1][0], edge[1][1]))
+
+        qp.end()
 
 
 def main():
-	app = QApplication(sys.argv)
-	widg = Test()
-	sys.exit(app.exec_())
+    app = QApplication(sys.argv)
+    window = Window()
+    sys.exit(app.exec_())
 
 
 if __name__ == '__main__':
-	main()
+    main()
